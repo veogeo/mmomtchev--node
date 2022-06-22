80b0d135004e2e0de9030cae87d9f54827db89e1
Momtchil Momtchev <momtchil@momtchev.com>
2022-06-22 22:27:22 +0000

    support interaction with the JS environment

--- test/embedding/napi_embedding.c
+++ test/embedding/napi_embedding.c
@@ -3,6 +3,7 @@
 #include <node_api.h>
 
 #include <stdio.h>
+#include <string.h>
 
 // Note: This file is being referred to from doc/api/embedding.md, and excerpts
 // from it are included in the documentation. Try to keep these in sync.
@@ -41,12 +42,61 @@ int RunNodeInstance(napi_platform platform) {
   napi_status r;
   napi_env env;
   int exit_code;
+  napi_handle_scope scope;
 
-  r = napi_create_environment(platform, NULL, main_script, &env);
-  if (r != napi_ok) {
+  if (napi_create_environment(platform, NULL, main_script, &env) != napi_ok) {
     fprintf(stderr, "Failed running JS\n");
     return -1;
   }
+
+  if (napi_open_handle_scope(env, &scope) != napi_ok) {
+    fprintf(stderr, "Failed creating a handle scope\n");
+    return -1;
+  }
+
+  napi_value global;
+  if (napi_get_global(env, &global) != napi_ok) {
+    fprintf(stderr, "Failed accessing the global object\n");
+    return -1;
+  }
+
+  napi_value cb;
+  napi_value key;
+  napi_create_string_utf8(env, "callMe", strlen("callMe"), &key);
+
+  if (napi_get_property(env, global, key, &cb) != napi_ok) {
+    fprintf(stderr, "Failed accessing the global object\n");
+    return -1;
+  }
+
+  napi_valuetype cb_type;
+  if (napi_typeof(env, cb, &cb_type) != napi_ok) {
+    fprintf(stderr, "Failed accessing the global object\n");
+    return -1;
+  }
+
+  if (cb_type == napi_function) {
+    napi_value undef;
+    napi_get_undefined(env, &undef);
+    napi_value arg;
+    napi_create_string_utf8(env, "called", strlen("called"), &arg);
+    napi_value result;
+    napi_call_function(env, undef, cb, 1, &arg, &result);
+
+    char buf[16];
+    size_t len;
+    napi_get_value_string_utf8(env, result, buf, 16, &len);
+    if (strncmp(buf, "called you", len)) {
+      fprintf(stderr, "Invalid value received: %s\n", buf);
+      return -1;
+    }
+    printf("%s", buf);
+  } else if (cb_type != napi_undefined) {
+    fprintf(stderr, "Invalid callMe value\n");
+    return -1;
+  }
+
+  napi_close_handle_scope(env, scope);
 
   r = napi_destroy_environment(env, &exit_code);
   if (r != napi_ok) return -1;
