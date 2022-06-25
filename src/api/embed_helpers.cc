0e30e1a3b23a6c2d4410eca73d1969691de47344
Momtchil Momtchev <momtchil@momtchev.com>
2022-06-25 19:40:48 +0000

    run the linters

--- src/api/embed_helpers.cc
+++ src/api/embed_helpers.cc
@@ -162,7 +162,7 @@ CommonEnvironmentSetup::~CommonEnvironmentSetup() {
     bool platform_finished = false;
     impl_->platform->AddIsolateFinishedCallback(
         isolate,
-        [](void* data) { *static_cast<bool*>(data) = true; },
+        [](void* data) { * static_cast<bool*>(data) = true; },
         &platform_finished);
     impl_->platform->UnregisterIsolate(isolate);
     isolate->Dispose();
