18f292587010e1dda0940447a99a3cd1dd7b0c78
Momtchil Momtchev <momtchil@momtchev.com>
2022-08-22 08:40:31 +0000

    default_bootstrap didn't make it

--- src/js_native_api.h
+++ src/js_native_api.h
@@ -125,7 +125,6 @@ NAPI_EXTERN napi_status NAPI_CDECL napi_run_environment(napi_env env);
 NAPI_EXTERN napi_status NAPI_CDECL napi_await_promise(napi_env env,
                                                      napi_value promise,
                                                      napi_value* result);
-NAPI_EXTERN const char* napi_default_bootstrap();
 #endif  // NAPI_EXPERIMENTAL
 NAPI_EXTERN napi_status NAPI_CDECL napi_create_function(napi_env env,
                                                         const char* utf8name,
